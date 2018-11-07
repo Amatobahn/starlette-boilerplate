92e91bd30e918df45d3e2a09602833fd07f698f2
Greg Amato <amatobahn@gmail.com>
2018-11-07 16:18:55 -0600

    Added note for adding static file support

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ def main():
     routes.add_route(Route("/form/", hello_world_form_data, ['POST']))
 
     app = StarletteServer(debug=True)
+    # app.add_static_file_directory("static")  # If you want to reference static file folder within project.
     app.add_routes(routes)
     app.run()
 
